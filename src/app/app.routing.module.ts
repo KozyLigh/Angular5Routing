baa07b25856bb5f00cb2a4c36cf2063d3543d939
Marko Kozjak <kozypato@gmail.com>
2017-12-28 13:23:25 +0100

    Can deactivate guard - unsaved changes

--- src/app/app.routing.module.ts
+++ src/app/app.routing.module.ts
@@ -8,6 +8,7 @@ import { ServerComponent } from './servers/server/server.component';
 import { RouterModule, Routes} from '@angular/router';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import {AuthGuard} from './auth.guard.service';
+import {CanDeactivateGuard} from './servers/edit-server/can-deactivate-guard.service';
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent},
@@ -18,7 +19,7 @@ const appRoutes: Routes = [
 //        canActivate: [AuthGuard],
         canActivateChild: [AuthGuard],
         component: ServersComponent, children: [
-        { path: ':id/edit', component: EditServerComponent},
+        { path: ':id/edit', component: EditServerComponent, canDeactivate: [CanDeactivateGuard]},
         { path: ':id', component: ServerComponent}
     ]},
     {path: 'not-found', component: PageNotFoundComponent},
